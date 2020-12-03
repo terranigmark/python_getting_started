ab7421773cf97ff57b208339194eee1db09e6d6b
Hector Vega <linnk99@gmail.com>
2020-12-02 22:34:41 -0500

    Docstrings and a comment added

--- words.py
+++ words.py
@@ -1,7 +1,21 @@
+"""[summary]
+
+Returns:
+    [type]: [description]
+"""
+
 import sys
 from urllib.request import urlopen
 
 def fetch_words(url):
+    """Fetch a list of words from a URL
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing the words from the document.
+    """
     story = urlopen(url)
     story_words = []
 
@@ -14,12 +28,22 @@ def fetch_words(url):
     return story_words
 
 def print_items(items):
+    """Print items one per line.
+
+    Args:
+        An iterable series of printable items.
+    """
     for item in items:
         print(item)
 
 def main(url):
+    """Print each word from a text document from at a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    """
     words = fetch_words(url)
     print_items(words)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1]) # The 0th arg is the module filename
